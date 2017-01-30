b33af1784fcb63af88af85379cb789cdcccff83e
rmsare <robertmsare@gmail.com>
2017-01-30 14:37:29 -0800

    Take filter half-width as parameter (default 3 cells)

--- calc_noiselevel.m
+++ calc_noiselevel.m
@@ -1,5 +1,8 @@
-function [m, s2] = calc_noiselevel(grid, de)
+function [m, s2] = calc_noiselevel(grid, de, half_wid)
 
+if(nargin < 3)
+    half_wid = 3;
+end
 %z = detrend(z);
 [ny, nx] = size(grid);
 %fs = 1/de;
@@ -10,14 +13,14 @@ function [m, s2] = calc_noiselevel(grid, de)
 %nxmax = ceil(nx/2);
 
 %tic
-Z = fft2(grid);
+Z = fftshift(fft2(grid));
 A = abs(Z);
 %fprintf('Compute FFT:\t\t%3.2f s\n', toc)
 
 cy = round(ny/2);
 cx = round(nx/2);
-i = (cy-100:cy+100)';
-j = (cx-100:cx+100)';
+i = (cy-half_wid:cy+half_wid)';
+j = (cx-half_wid:cx+half_wid)';
 [I, J] = meshgrid(i,j);
 center_ind = sub2ind(size(Z), I(:), J(:));
 Zfilt = Z;
@@ -25,11 +28,11 @@ Zfilt(center_ind) = 0;
 
 %fprintf('Apply filter:\t\t%3.2f s\n', toc)
 
-zfilt = fftshift(ifft2(Zfilt));
+zfilt = ifft2(Zfilt);
 %fprintf('Compute IFFT:\t\t%3.2f s\n', toc)
 
-m = nanmean(zfilt(:));
-s2 = nanstd(zfilt(:));
+m = nanmean(abs(zfilt(:)));
+s2 = nanstd(abs(zfilt(:)));
 %fprintf('Calculate statistics:\t%3.2f s\n', toc)
 
 end
