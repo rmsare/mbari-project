079a70e322a80fbe1c71e9bb1bc338b36f86eef0
rmsare <robertmsare@gmail.com>
2017-02-13 13:24:16 -0800

    Use radially symmetric filter and 360 deg grid search

--- calc_noiselevel.m
+++ calc_noiselevel.m
@@ -1,4 +1,4 @@
-function [m, s2] = calc_noiselevel(grid, de, half_wid)
+function [m, s2, zfilt, Zfilt] = calc_noiselevel(grid, de, half_wid)
 
 if(nargin < 3)
     half_wid = 3;
@@ -14,25 +14,30 @@ end
 
 %tic
 Z = fftshift(fft2(grid));
-A = abs(Z);
 %fprintf('Compute FFT:\t\t%3.2f s\n', toc)
 
-cy = round(ny/2);
-cx = round(nx/2);
-i = (cy-half_wid:cy+half_wid)';
-j = (cx-half_wid:cx+half_wid)';
-[I, J] = meshgrid(i,j);
-center_ind = sub2ind(size(Z), I(:), J(:));
+%cy = round(ny/2);
+%cx = round(nx/2);
+%i = (cy-half_wid:cy+half_wid)';
+%j = (cx-half_wid:cx+half_wid)';
+%[I, J] = meshgrid(i,j);
+%center_ind = sub2ind(size(Z), I(:), J(:));
+
+x = (-floor(nx/2):floor(nx/2)-1);
+y = (-floor(ny/2):floor(ny/2)-1);
+[X,Y] = meshgrid(x, y);
+[TH, R] = cart2pol(X,Y);
+center_ind = find(R < half_wid);
+
 Zfilt = Z;
 Zfilt(center_ind) = 0;
-
 %fprintf('Apply filter:\t\t%3.2f s\n', toc)
 
-zfilt = ifft2(Zfilt);
+zfilt = ifft2(fftshift(Zfilt));
 %fprintf('Compute IFFT:\t\t%3.2f s\n', toc)
 
-m = nanmean(abs(zfilt(:)));
-s2 = nanstd(abs(zfilt(:)));
+m = nanmean(real(zfilt(:)));
+s2 = nanstd(real(zfilt(:)));
 %fprintf('Calculate statistics:\t%3.2f s\n', toc)
 
 end
